fe98dccbc11f0c4980e44c53fca9bc3c3f1e8f72
Bill <bill@anantya.com>
2015-04-16 16:51:56 +0800

    sort out path issues

--- Extension.php
+++ Extension.php
@@ -13,11 +13,19 @@ class Extension extends BaseExtension
         if ( $this->config and isset($this->config['highlightjs_style'] ) ) {
             $styleName = $this->config['highlightjs_style'];
         }      
-        $path = $this->app['paths']['extensions'].'vendor/billbsing/'.$this->getName().'/'; 
-        $this->addCss('assets/styles/'.$styleName.'.css');
-//        $this->addJavascript($path.'assets/highlight.pack.js');
-        $this->addSnippet('endofbody', '<script src="'.$path.'assets/highlight.pack.js"></script>' 
-                 . '<script>hljs.initHighlightingOnLoad();</script>');
+        
+        $assetsPath = $this->getBasePath().'/assets';
+
+        // check to see if we can find the assets in our extension path
+        if ( file_exists($assetsPath) ) {
+            $this->addCss('assets/styles/'.$styleName.'.css');
+    
+            // need to make sure that the init script always runs after the js load script
+            
+//          $this->addJavascript($path.'assets/highlight.pack.js');
+            $this->addSnippet('endofbody', '<script src="'.$this->getBaseUrl().'assets/highlight.pack.js"></script>' 
+                     . '<script>hljs.initHighlightingOnLoad();</script>');
+        }
     }
 
     public function getName()
